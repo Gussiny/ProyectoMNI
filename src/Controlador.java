3e507d4221e5282ebe291881a945e7a4f056339d
Ricardope3 <31715322+Ricardope3@users.noreply.github.com>
2018-11-08 08:10:23 -0600

    ya no da error al dejar una casilla vacía

--- src/Controlador.java
+++ src/Controlador.java
@@ -15,40 +15,40 @@ public class Controlador {
             this.vista.addCalcularListener(new ActionListener() {
                 @SuppressWarnings("unused")
 				public void actionPerformed(ActionEvent e) {
-                	
-                    Double a = Double.parseDouble(vista.textA.getText());
-                    Double b = Double.parseDouble(vista.textB.getText());
-                    Double tol = Double.parseDouble(vista.textTol.getText());
-                  
-                    if(a<tol){
-                        this.MuestraError("Valores de a es menor a la tolerancia");
-                    }
-                    if(b <tol){
-                        this.MuestraError("Valores de b es menor a la tolerancia");
-                    }
-                    Double x = 0.0;
-                    try{
-                        x = modelo.regresarX(a,b,tol);
-                    }
-                    catch(InvalidParameterException ex){
-                        this.MuestraError("No hay valores criticos reales");
-                    }
-                    if(x==null){
-                        this.MuestraError("No hay valores mÃ¡xmimos para los valores de a y b dados");
-                    }
-                    else if(x!=null){
-                        vista.result.setText("Se necesita que 'x' sea de tamaño: " + String.valueOf(Math.round(x*10000.0)/10000.0));
-                    }
-                    else{
-                        vista.result.setText("Indefinido");
-                    }
-                    
-                }
-                 
+                	try {
+                		Double a = Double.parseDouble(vista.textA.getText());
+                        Double b = Double.parseDouble(vista.textB.getText());
+                        Double tol = Double.parseDouble(vista.textTol.getText()); 
+                        if(a<tol){
+                            this.MuestraError("Valores de a es menor a la tolerancia");
+                        }
+                        if(b <tol){
+                            this.MuestraError("Valores de b es menor a la tolerancia");
+                        }
+                        Double x = 0.0;
+                        try{
+                            x = modelo.regresarX(a,b,tol);
+                        }
+                        catch(InvalidParameterException ex){
+                            this.MuestraError("No hay valores criticos reales");
+                        }
+                        if(x==null){
+                            this.MuestraError("No hay valores mÃ¡xmimos para los valores de a y b dados");
+                        }
+                        else if(x!=null){
+                            vista.result.setText("Se necesita que 'x' sea de tamaño: " + String.valueOf(Math.round(x*10000.0)/10000.0));
+                        }
+                        else{
+                            vista.result.setText("Indefinido");
+                        }
+                	}
+                	catch(NumberFormatException exc) {
+                		this.MuestraError("Por favor no deje ninguna casilla vacía");
+                	}                    
+                }                 
                 public void MuestraError(String string){
                     vista.error.setText(string);
-                }
-                
+                }           
             });
         }
 
