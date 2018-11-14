c7264749d05f45110f34c4b4003e92e32104ddd2
Gussiny <37113826+Gussiny@users.noreply.github.com>
2018-11-13 23:57:51 -0600

    cosas fake

--- src/Vista.java
+++ src/Vista.java
@@ -9,25 +9,25 @@ public class Vista extends JFrame{
 	public JLabel titulo = new JLabel();
 	public JLabel labelA = new JLabel(" a:  ");
 	public JLabel labelB = new JLabel(" b:  ");
-	public JLabel labelTol = new JLabel("Tol: ");
+	public JLabel labelTol = new JLabel("Tolerancia: ");
 	public JTextField textA = new JTextField();
 	public JTextField textB = new JTextField();
 	public JTextField textTol = new JTextField();
-	
+
 	public JPanel panelA = new JPanel();
 	public JPanel panelB = new JPanel();
 	public JPanel panelTol = new JPanel();
 	public JPanel panelX = new JPanel();
 	public JPanel panelBtCalcular = new JPanel();
 	public JPanel panelUnidades = new JPanel();
-	
+
 	public JButton bCalcular = new JButton("Calcular");
 	public JLabel result = new JLabel();
 	public JLabel error = new JLabel();
 	public JLabel panelMedidaA = new JLabel(" m");
 	public JLabel panelMedidaB = new JLabel(" m");
 	public JComboBox<String> unidadesMedicion = new JComboBox<String>();
-	
+
 	private PanelDibujo pd;
 
 	public Vista(PanelDibujo pd2){
@@ -46,7 +46,7 @@ public class Vista extends JFrame{
 		panelControl.setLayout(new BoxLayout(panelControl,BoxLayout.Y_AXIS));
 		panelControl.setPreferredSize(new Dimension(200, 120));
 		this.inicarCombo();
-		
+
 		panelA.setLayout(new GridBagLayout());
 		panelB.setLayout(new GridBagLayout());
 		panelTol.setLayout(new GridBagLayout());
@@ -61,26 +61,26 @@ public class Vista extends JFrame{
 		result.setFont(new Font("Arial",Font.BOLD, 26));
 		result.setText("X = ");
 		//error.setText("No hay error");
-		
+
 		panelUnidades.add(unidadesMedicion);
 
 		panelA.add(labelA);
 		panelA.add(textA);
 		panelA.add(panelMedidaA);
-		
+
 		panelB.add(labelB);
 		panelB.add(textB);
 		panelB.add(panelMedidaB);
-		
+
 		panelTol.add(labelTol);
 		panelTol.add(textTol);
 		textTol.setText("0.001");
-		
+
 		panelX.add(result);
-		
+
 		panelBtCalcular.add(bCalcular);
-		
-		
+
+
 		//panelControl.add(titulo);
 		panelControl.add(panelUnidades);
 		panelControl.add(panelA);
@@ -97,10 +97,10 @@ public class Vista extends JFrame{
 		this.panelTol.setBackground(Color.LIGHT_GRAY);
 		this.panelBtCalcular.setBackground(Color.LIGHT_GRAY);
 		this.panelX.setBackground(Color.WHITE);
-		
+
 		this.pd = pd2;
 		this.add(pd,BorderLayout.CENTER);
-				
+
 		panelControl.setBackground(Color.LIGHT_GRAY);
 		this.add(panelControl,BorderLayout.EAST);
 		this.setVisible(true);
@@ -110,7 +110,7 @@ public class Vista extends JFrame{
 	public void addCalcularListener(ActionListener actionListener) {
 		this.bCalcular.addActionListener(actionListener);
 	}
-	
+
 	public void inicarCombo() {
 		this.unidadesMedicion.addItem("m");
 		this.unidadesMedicion.addItem("cm");
@@ -118,7 +118,7 @@ public class Vista extends JFrame{
 		this.unidadesMedicion.addItem("ft");
 		this.unidadesMedicion.addItem("in");
 		this.unidadesMedicion.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				panelMedidaA.setText(" " + unidadesMedicion.getSelectedItem().toString());
@@ -127,6 +127,6 @@ public class Vista extends JFrame{
 				pd.repaint();
 			}
 		});
-		
+
 	}
 }
